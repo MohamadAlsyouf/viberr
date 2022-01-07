70e5a4a35517eedba05e694bb54b7b64c95a73c5
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-01-06 16:00:20 -0800

    Create an XMLHttpRequest object to get the data(artists and video) when the user submits search

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var $genreList = document.querySelector('.genre-list');
 var $searchPage = document.querySelector('.search-page');
+var $searchForm = document.querySelector('#search-form');
 
 function handleGenrePress(event) {
   var dataViewValue = event.target.getAttribute('data-view');
@@ -9,3 +10,15 @@ function handleGenrePress(event) {
   }
 }
 document.addEventListener('click', handleGenrePress);
+
+function handleSubmit(event) {
+  var name = $searchForm.elements.q.value;
+  var xhr = new XMLHttpRequest();
+  var targetUrl = encodeURIComponent('https://tastedive.com/api/similar?k=429885-Bootcamp-CY4UKJN5&limit=10&info=1&q=');
+  xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl + name);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+  });
+  xhr.send();
+}
+$searchForm.addEventListener('submit', handleSubmit);
