9a0982403f18ac5ba54ae8bdd5c37e6b24aeedea
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-01-07 17:21:35 -0800

    Create a function that generates the DOM tree for each artist in the data.artists array and append the new DOM elements to the div with the class='results-row'

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ var $genreList = document.querySelector('.genre-list');
 var $searchPage = document.querySelector('.search-page');
 var $searchForm = document.querySelector('#search-form');
 var $vibesMessage = document.querySelector('.vibes-message');
-// var $ul = document.querySelector('ul');
-// var $li = document.querySelector('li');
+var $resultsRow = document.querySelector('.results-row');
 
 function handleGenrePress(event) {
   var dataViewValue = event.target.getAttribute('data-view');
@@ -18,76 +17,55 @@ function handleSubmit(event) {
   $vibesMessage.textContent = 'Similar Vibes';
   var name = $searchForm.elements.q.value;
   var xhr = new XMLHttpRequest();
-  var targetUrl = encodeURIComponent('https://tastedive.com/api/similar?k=429885-Bootcamp-CY4UKJN5&limit=10&info=1&q=');
+  var targetUrl = encodeURIComponent('https://tastedive.com/api/similar?k=429885-Bootcamp-CY4UKJN5&limit=10&type=music&info=1&q=');
   xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl + name);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log(xhr.status);
-    // console.log(xhr.response);
-
     data.artists = xhr.response.Similar.Results;
-    // data.artists.forEach(createList);
-    // check code journal appendDom function
+    appendDOM(data.artists);
   });
   xhr.send();
 }
 
-// function generateArtistDOM(object) {
-//   var threeArtistFullColumn = document.createElement('div');
-//   threeArtistFullColumn.className = 'column-full';
-//   $li.appendChild(threeArtistFullColumn);
+function generateArtistDOM(object) {
+  // var threeArtistFullColumn = document.createElement('div');
+  // threeArtistFullColumn.className = 'column-full';
+  // $li.appendChild(threeArtistFullColumn);
 
-//   var threeArtistRow = document.createElement('div');
-//   threeArtistRow.className = 'row';
-//   threeArtistFullColumn.appendChild(threeArtistRow);
+  // var threeArtistRow = document.createElement('div');
+  // threeArtistRow.className = 'row';
+  // threeArtistFullColumn.appendChild(threeArtistRow);
 
-//   var artistBox = document.createElement('div');
-//   artistBox.className = 'column-third video-box';
-//   threeArtistRow.appendChild(artistBox);
+  var artistBox = document.createElement('div');
+  artistBox.className = 'column-third video-box';
+  $resultsRow.appendChild(artistBox);
 
-//   var artistName = document.createElement('p');
-//   artistName.className = 'artist-name';
-//   artistName.textContent = object.name; // Name maybe? instead of name
-//   artistBox.appendChild(artistName);
+  var artistName = document.createElement('p');
+  artistName.className = 'artist-name';
+  artistName.textContent = object.Name; // Name maybe? instead of name
+  artistBox.appendChild(artistName);
 
-//   var artistVideo = document.createElement('iFrame');
-// }
+  var artistVideo = document.createElement('iFrame');
+  artistVideo.className = 'column-full';
+  artistVideo.setAttribute('height', '205');
+  artistVideo.setAttribute('src', object.yUrl);
+  artistBox.appendChild(artistVideo);
 
-// function generateArtistsDOM(object) {
-// // describes what i want to do to just one item in the array
+}
 
-//   if (object.yUrl === null) {
-//     return;
-//   }
-
-//   // var artistLi = document.createElement('li');
-//   // artistLi.className = 'artist-li';
-//   // artistLi.setAttribute('data-entry-id', data.nextArtistId);
-
-//   var artistRow = document.createElement('div');
-//   artistRow.className = 'row';
-//   $ul.appendChild(artistRow);
-
-//   var artistArea = document.createElement('div');
-//   artistArea.className = 'column-full';
-//   artistRow.appendChild(artistArea);
-
-//   var artistBox = document.createElement('div');
-//   artistBox.className = 'column-third';
-//   artistRow.appendChild(artistBox);
-
-//   var artistName = document.createElement('p');
-//   artistName.textContent = object.Name;
-//   artistName.className = 'artist-name';
-//   artistBox.appendChild(artistName);
-
-//   var artistVideo = document.createElement('a');
-//   artistVideo.setAttribute('href', object.yUrl);
-//   artistVideo.textContent = object.yUrl;
-//   artistBox.appendChild(artistVideo);
-
-//   // return artistRow;
-// }
+function appendDOM(object) {
+  if (object.yUrl === null) {
+    return;
+  }
+  for (var i = 0; i < data.artists.length; i++) {
+    if (data.artists[i].yUrl === null) {
+      return;
+    }
+    generateArtistDOM(data.artists[i]);
+    // console.log(data.artists[i]);
+  }
+}
 
 // event listeners
 $searchForm.addEventListener('submit', handleSubmit);
+document.addEventListener('DOMContentLoaded', appendDOM);
