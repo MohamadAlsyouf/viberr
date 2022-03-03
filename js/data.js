03eb7eee463e715be10c4fd05c07b9426493aa6f
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-03-03 15:33:20 -0800

    Created an artists link on nav bar

--- js/data.js
+++ js/data.js
@@ -5,3 +5,17 @@ var data = {
   editing: null,
   nextArtistId: 1
 };
+
+// new entries are saved to local storage along w rest of data model on reload
+
+var previousArtist = localStorage.getItem('artist-data');
+if (previousArtist !== null) {
+  data = JSON.parse(previousArtist);
+}
+
+function handleBeforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('artist-data', dataJSON);
+}
+
+window.addEventListener('beforeunload', handleBeforeUnload);
