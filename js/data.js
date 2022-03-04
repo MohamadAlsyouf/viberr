c5a66ce5f44527036eadb470fb20f5f241ac31aa
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-03-04 11:31:57 -0800

    generated DOM of liked artists onto the page from the data.likedArtists array

--- js/data.js
+++ js/data.js
@@ -7,16 +7,17 @@ var data = {
   nextArtistId: 1
 };
 
-// new entries are saved to local storage along w rest of data model on reload
+// var dataJSON = localStorage.getItem('artist-data');
+// if (dataJSON !== null) {
+//   data = JSON.parse(dataJSON);
+// }
 
-var previousArtist = localStorage.getItem('artist-data');
-if (previousArtist !== null) {
-  data = JSON.parse(previousArtist);
-}
+// function handleBeforeUnload(event) {
+//   data.artists = [];
+//   var dataJSON = JSON.stringify(data);
+//   localStorage.setItem('artist-data', dataJSON);
+// }
 
-function handleBeforeUnload(event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('artist-data', dataJSON);
-}
+// window.addEventListener('beforeunload', handleBeforeUnload);
 
-window.addEventListener('beforeunload', handleBeforeUnload);
+// FOR PAGE TO STAY ON RELOAD, I NEED TO KEEP TRACK OF THE CURRENT VIEW IN MY DATA MODEL ABOVE, AND SET THAT IN MY BEFOREUNLOAD FUNCTION UNDER DATA.ARTISTS = []
--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ var $views = document.querySelectorAll('.view');
 var $genreList = document.querySelector('#genre-list');
 var $navLogo = document.querySelector('#logo-img');
 var $appName = document.querySelector('.app-name');
+var $likedArtists = document.querySelector('.artist-nav');
+// append the liked artists DOM to this below.
+var $likedArtistsRow = document.querySelector('.liked-artists-row');
 
 // view swapping function
 function swapViews(view) {
@@ -26,14 +29,24 @@ function showHomePage() {
   swapViews('home-page');
 }
 
+function showLikedArtists() {
+  swapViews('liked-artists');
+}
+
 // handle liked Artists function
+
+// LIKED ARTISTS ARE RENDERING WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function likeArtist(event) {
+  // console.log(event.target);
   var dataArtistIdNum = parseInt(event.target.getAttribute('data-artist-id'));
-  for (var i = 0; i < data.artists.length; i++) {
-    if (dataArtistIdNum === data.artists[i].id) {
-      $resultsRow.children[i].remove();
-      data.likedArtists.push(data.artists[i]);
-      data.artists.splice(data.artists[i], 1);
+  if (event.target.className === 'fas fa-plus') {
+    for (var i = 0; i < data.artists.length; i++) {
+      if (dataArtistIdNum === data.artists[i].id) {
+        var theLikedArtistDOM = generateLikedArtistsDOM(data.artists[i]);
+        $likedArtistsRow.appendChild(theLikedArtistDOM);
+        $resultsRow.children[i].remove();
+        data.likedArtists.push(data.artists[i]);
+      }
     }
   }
 }
@@ -104,41 +117,52 @@ function appendDOM(object) {
 }
 
 // generate DOM function for Liked Artists
-// function generateLikedArtistsDOM(object) {
+function generateLikedArtistsDOM(artistObject) {
 
-//   var artistBox = document.createElement('div');
-//   artistBox.className = 'column-third video-box';
-//   $resultsRow.appendChild(artistBox);
+  var artistBox = document.createElement('div');
+  artistBox.className = 'column-third video-box';
+  $likedArtistsRow.appendChild(artistBox);
 
-//   var artistName = document.createElement('p');
-//   artistName.className = 'artist-name';
-//   artistName.textContent = object.Name;
-//   artistBox.appendChild(artistName);
+  var artistName = document.createElement('p');
+  artistName.className = 'artist-name';
+  artistName.textContent = artistObject.Name;
+  artistBox.appendChild(artistName);
 
-//   var artistVideo = document.createElement('iFrame');
-//   artistVideo.className = 'column-full';
-//   artistVideo.setAttribute('height', '205');
-//   artistVideo.setAttribute('src', object.yUrl);
-//   artistBox.appendChild(artistVideo);
+  // !!!!!!!!!!!!!!!!!!!!!! MAKE THIS A TRASH CAN ICON !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // var plusIcon = document.createElement('i');
+  // plusIcon.className = 'fas fa-plus';
+  // plusIcon.setAttribute('data-artist-id', artistObject.id);
+  // artistName.appendChild(plusIcon);
 
-//   return artistBox;
-// }
+  var artistVideo = document.createElement('iFrame');
+  artistVideo.className = 'column-full';
+  artistVideo.setAttribute('height', '205');
+  artistVideo.setAttribute('src', artistObject.yUrl);
+  artistBox.appendChild(artistVideo);
 
-// append LIKED Artists DOM function
-// function appendLikedArtistsDOM(object) {
-//   for (var i = 0; i < data.likedArtists.length; i++) {
-//     if (data.artists[i].yUrl === null) {
-//       continue;
-//     } else {
-//       generateLikedArtistsDOM(data.likedArtists[i]);
-//     }
-//   }
-// }
+  return artistBox;
+}
 
 // event listeners
 $searchForm.addEventListener('submit', handleSubmit);
-document.addEventListener('DOMContentLoaded', appendDOM);
+document.addEventListener('DOMContentLoaded', () => {
+  appendDOM();
+  for (var i = 0; i < data.likedArtists.length; i++) {
+    var theLikedArtistDOM = generateLikedArtistsDOM(data.likedArtists[i]);
+    $likedArtistsRow.appendChild(theLikedArtistDOM);
+  }
+});
 $genreList.addEventListener('click', showArtistSearch);
 $navLogo.addEventListener('click', showHomePage);
 $appName.addEventListener('click', showHomePage);
 document.addEventListener('click', likeArtist);
+$likedArtists.addEventListener('click', showLikedArtists);
+
+//
+//
+//
+// ISSUES !!!!!!! RENDERING WRONG AMOUNT OF VIDEOS INTO LIKED ARTISTS VIEW
+//                NOT CONDITIONALLY STORING LOCAL STORAGE
+//                VIEW CHANGES/DOES NOT RESET CORRECTLY ON VIEW RELOAD
+//                FIX ARTISTS LINK ITS HELLLA UGLY*
+//                LIKED ARTISTS DO NOT SAVE ON PAGE RELOAD!!!
