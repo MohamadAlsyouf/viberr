a52f616441025af0a86b042262b84cd9799f814a
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-03-04 11:46:30 -0800

    Created a function for saving liked artist data to local storage

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 /* exported data */
-var data = {
+let data = {
   view: 'genres',
   artists: [],
   likedArtists: [],
@@ -7,17 +7,17 @@ var data = {
   nextArtistId: 1
 };
 
-// var dataJSON = localStorage.getItem('artist-data');
-// if (dataJSON !== null) {
-//   data = JSON.parse(dataJSON);
-// }
+const dataJSON = localStorage.getItem('artist-data');
+if (dataJSON !== null) {
+  data = JSON.parse(dataJSON);
+}
 
-// function handleBeforeUnload(event) {
-//   data.artists = [];
-//   var dataJSON = JSON.stringify(data);
-//   localStorage.setItem('artist-data', dataJSON);
-// }
+const handleBeforeUnload = event => {
+  data.artists = [];
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('artist-data', dataJSON);
+};
 
-// window.addEventListener('beforeunload', handleBeforeUnload);
+window.addEventListener('beforeunload', handleBeforeUnload);
 
 // FOR PAGE TO STAY ON RELOAD, I NEED TO KEEP TRACK OF THE CURRENT VIEW IN MY DATA MODEL ABOVE, AND SET THAT IN MY BEFOREUNLOAD FUNCTION UNDER DATA.ARTISTS = []
--- js/main.js
+++ js/main.js
@@ -1,109 +1,104 @@
-var $searchForm = document.querySelector('#search-form');
-var $vibesMessage = document.querySelector('.vibes-message');
-var $resultsRow = document.querySelector('.results-row');
-var $views = document.querySelectorAll('.view');
+const $searchForm = document.querySelector('#search-form');
+const $vibesMessage = document.querySelector('.vibes-message');
+const $resultsRow = document.querySelector('.results-row');
+const $views = document.querySelectorAll('.view');
 
-var $genreList = document.querySelector('#genre-list');
-var $navLogo = document.querySelector('#logo-img');
-var $appName = document.querySelector('.app-name');
-var $likedArtists = document.querySelector('.artist-nav');
-// append the liked artists DOM to this below.
-var $likedArtistsRow = document.querySelector('.liked-artists-row');
+const $genreList = document.querySelector('#genre-list');
+const $navLogo = document.querySelector('#logo-img');
+const $appName = document.querySelector('.app-name');
+const $likedArtists = document.querySelector('.artist-nav');
+const $likedArtistsRow = document.querySelector('.liked-artists-row');
 
 // view swapping function
-function swapViews(view) {
-  for (var i = 0; i < $views.length; i++) {
+const swapViews = view => {
+  for (let i = 0; i < $views.length; i++) {
     if ($views[i].getAttribute('data-view') === view) {
       $views[i].classList.remove('hidden');
     } else {
       $views[i].classList.add('hidden');
     }
   }
-}
+};
 
-function showArtistSearch() {
+const showArtistSearch = () => {
   swapViews('artist-search');
-}
+};
 
-function showHomePage() {
+const showHomePage = () => {
   swapViews('home-page');
-}
+};
 
-function showLikedArtists() {
+const showLikedArtists = () => {
   swapViews('liked-artists');
-}
+};
 
 // handle liked Artists function
-
-// LIKED ARTISTS ARE RENDERING WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-function likeArtist(event) {
-  // console.log(event.target);
-  var dataArtistIdNum = parseInt(event.target.getAttribute('data-artist-id'));
+const likeArtist = event => {
+  const dataArtistIdNum = parseInt(event.target.getAttribute('data-artist-id'));
   if (event.target.className === 'fas fa-plus') {
-    for (var i = 0; i < data.artists.length; i++) {
+    for (let i = 0; i < data.artists.length; i++) {
       if (dataArtistIdNum === data.artists[i].id) {
-        var theLikedArtistDOM = generateLikedArtistsDOM(data.artists[i]);
+        const theLikedArtistDOM = generateLikedArtistsDOM(data.artists[i]);
         $likedArtistsRow.appendChild(theLikedArtistDOM);
         $resultsRow.children[i].remove();
         data.likedArtists.push(data.artists[i]);
       }
     }
   }
-}
+};
 
 // handle submit function
-function handleSubmit(event) {
+const handleSubmit = event => {
   event.preventDefault();
   $vibesMessage.textContent = 'Similar Vibes';
-  var name = $searchForm.elements.q.value;
-  var xhr = new XMLHttpRequest();
-  var targetUrl = encodeURIComponent('https://tastedive.com/api/similar?k=429885-Bootcamp-CY4UKJN5&limit=10&type=music&info=1&q=');
+  const name = $searchForm.elements.q.value;
+  const xhr = new XMLHttpRequest();
+  const targetUrl = encodeURIComponent('https://tastedive.com/api/similar?k=429885-Bootcamp-CY4UKJN5&limit=10&type=music&info=1&q=');
   xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl + name);
   xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-    var similarArtists = xhr.response.Similar.Results;
-    for (var i in similarArtists) {
+  xhr.addEventListener('load', () => {
+    const similarArtists = xhr.response.Similar.Results;
+    for (const i in similarArtists) {
       similarArtists[i].id = data.nextArtistId;
       data.nextArtistId++;
     }
-
     data.artists = similarArtists;
     appendDOM(data.artists);
   });
   xhr.send();
-}
+};
 
 // generate DOM function for Artist Search
-function generateArtistDOM(artistObject) {
+const generateArtistDOM = artistObject => {
 
-  var artistBox = document.createElement('div');
+  const artistBox = document.createElement('div');
   artistBox.className = 'column-third video-box';
   artistBox.setAttribute('data-artist-id', artistObject.id);
   $resultsRow.appendChild(artistBox);
 
-  var artistName = document.createElement('p');
+  const artistName = document.createElement('p');
   artistName.className = 'row artist-name';
   artistName.textContent = artistObject.Name;
   artistBox.appendChild(artistName);
 
-  var plusIcon = document.createElement('i');
+  const plusIcon = document.createElement('i');
   plusIcon.className = 'fas fa-plus';
   plusIcon.setAttribute('data-artist-id', artistObject.id);
   artistName.appendChild(plusIcon);
 
-  var artistVideo = document.createElement('iFrame');
+  const artistVideo = document.createElement('iFrame');
   artistVideo.className = 'column-full';
   artistVideo.setAttribute('height', '205');
   artistVideo.setAttribute('src', artistObject.yUrl);
   artistBox.appendChild(artistVideo);
 
   return artistBox;
-}
+};
 
 // append Artist Search DOM function
-function appendDOM(object) {
-  var validArtists = 0;
-  for (var i = 0; i < data.artists.length; i++) {
+const appendDOM = object => {
+  let validArtists = 0;
+  for (let i = 0; i < data.artists.length; i++) {
     if (data.artists[i].yUrl === null) {
       continue;
     } else {
@@ -114,16 +109,16 @@ function appendDOM(object) {
       return;
     }
   }
-}
+};
 
 // generate DOM function for Liked Artists
-function generateLikedArtistsDOM(artistObject) {
+const generateLikedArtistsDOM = artistObject => {
 
-  var artistBox = document.createElement('div');
+  const artistBox = document.createElement('div');
   artistBox.className = 'column-third video-box';
   $likedArtistsRow.appendChild(artistBox);
 
-  var artistName = document.createElement('p');
+  const artistName = document.createElement('p');
   artistName.className = 'artist-name';
   artistName.textContent = artistObject.Name;
   artistBox.appendChild(artistName);
@@ -134,21 +129,21 @@ function generateLikedArtistsDOM(artistObject) {
   // plusIcon.setAttribute('data-artist-id', artistObject.id);
   // artistName.appendChild(plusIcon);
 
-  var artistVideo = document.createElement('iFrame');
+  const artistVideo = document.createElement('iFrame');
   artistVideo.className = 'column-full';
   artistVideo.setAttribute('height', '205');
   artistVideo.setAttribute('src', artistObject.yUrl);
   artistBox.appendChild(artistVideo);
 
   return artistBox;
-}
+};
 
 // event listeners
 $searchForm.addEventListener('submit', handleSubmit);
 document.addEventListener('DOMContentLoaded', () => {
   appendDOM();
-  for (var i = 0; i < data.likedArtists.length; i++) {
-    var theLikedArtistDOM = generateLikedArtistsDOM(data.likedArtists[i]);
+  for (let i = 0; i < data.likedArtists.length; i++) {
+    const theLikedArtistDOM = generateLikedArtistsDOM(data.likedArtists[i]);
     $likedArtistsRow.appendChild(theLikedArtistDOM);
   }
 });
@@ -161,8 +156,4 @@ $likedArtists.addEventListener('click', showLikedArtists);
 //
 //
 //
-// ISSUES !!!!!!! RENDERING WRONG AMOUNT OF VIDEOS INTO LIKED ARTISTS VIEW
-//                NOT CONDITIONALLY STORING LOCAL STORAGE
-//                VIEW CHANGES/DOES NOT RESET CORRECTLY ON VIEW RELOAD
-//                FIX ARTISTS LINK ITS HELLLA UGLY*
-//                LIKED ARTISTS DO NOT SAVE ON PAGE RELOAD!!!
+// ISSUES !!!!!!!
